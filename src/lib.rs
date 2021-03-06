1075425a50c48af72a4bf77fdf23053007beebdd
DevHyperCoder <arduinoleo88@gmail.com>
2021-03-06 11:37:52 +0530

    ✨ Add alphabetic character conversion

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod constants;
 
 use std::io::stdin;
+use constants::get_terms;
 
 pub fn run() {
     println!("ALPC");
@@ -12,4 +13,37 @@ pub fn run() {
     stdin().read_line(&mut input_str).unwrap();
 
     println!("Converting: {}",input_str);
+
+    let result = parse(input_str);
+
+    println!("{:?}",result);
+}
+
+fn parse(input:String) -> Vec<String>{
+    let input = input.to_ascii_uppercase();
+    let mut result = Vec::new();
+
+    // Loop through chars ; Append to result vector
+    for c in input.chars() {
+        // Space to newline
+        if c == ' ' {
+            result.push(format!("{}","\n"));
+            continue;
+        }
+
+        if !c.is_alphabetic(){
+            result.push(format!("{} ",c));
+            continue;
+        }
+
+        let index = c as usize;
+
+        if index < 65 {continue; }
+
+        // ASCI reper to array index
+        let index = index - 65;
+
+        result.push(format!("{} ",get_terms()[index]));
+    }
+    result
 }
