6ab67cd61c25183a58593a1b9cc9b81735802820
DevHyperCoder <arduinoleo88@gmail.com>
2021-03-06 11:19:27 +0530

    💬 Add terms and numbers

---
+++ src/constants.rs
@@ -0,0 +1,13 @@
+pub fn get_terms() -> Vec<&'static str> {
+    vec![
+        "Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India",
+        "Juliet", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra",
+        "Tango", "Uniform", "Victor", "Whiskey", "X-ray", "Yankee", "Zulu",
+    ]
+}
+
+pub fn get_numbers() -> Vec<&'static str> {
+    vec![
+        "zero", "one", "two", "tree", "four", "fife", "six", "seven", "eight", "niner",
+    ]
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+mod constants;
+
 pub fn run() {
     println!("ALPC");
+    println!("{:?}", constants::get_terms());
+    println!("{:?}", constants::get_numbers());
 }
