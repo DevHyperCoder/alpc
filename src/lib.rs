572827da9132ca54a2fe1e8599b20f42bf7c8b2b
DevHyperCoder <arduinoleo88@gmail.com>
2021-03-06 11:22:21 +0530

    Read from stdin

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
 mod constants;
 
+use std::io::stdin;
+
 pub fn run() {
     println!("ALPC");
     println!("{:?}", constants::get_terms());
     println!("{:?}", constants::get_numbers());
+
+    let mut input_str = String::new();
+
+    stdin().read_line(&mut input_str).unwrap();
+
+    println!("Converting: {}",input_str);
 }
